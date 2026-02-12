e40e5f9e4526311d800f1774fc1426efe3701a3c
Dongmo Dassi Raissa Dorlane <dassiraissa10@gmail.com>
2026-02-12 15:25:09 +0100

    Fix casing for Skills component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { motion, useScroll, useSpring } from 'framer-motion';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
-import Skills from './components/Skills';
+import Skills from "./components/SkillsTem";
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 
